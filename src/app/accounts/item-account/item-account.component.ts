dd6375288f926f79908ecd48751d9c11f122f9be
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-03-14 22:45:41 +0100

    etape 1 exercice accounts

---
+++ src/app/accounts/item-account/item-account.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-account',
+  imports: [],
+  templateUrl: './item-account.component.html',
+  styleUrl: './item-account.component.css'
+})
+export class ItemAccountComponent {
+
+}
