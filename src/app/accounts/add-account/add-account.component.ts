8aa2e696fe1338ad80a26b1f043c5e3e12c1e7ae
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-03-21 21:18:55 +0100

    correction exercice accounts

--- src/app/accounts/add-account/add-account.component.ts
+++ src/app/accounts/add-account/add-account.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-account',
   imports: [],
   templateUrl: './add-account.component.html',
-  styleUrl: './add-account.component.css'
+  styleUrl: './add-account.component.css',
 })
 export class AddAccountComponent {
+  allOptions = ['', 'active', 'inactive', 'unknown'];
+  @Output() eventToHomeAccount = new EventEmitter();
 
+  addHandler(nameValue, selectValue) {
+    this.eventToHomeAccount.emit({
+      name: nameValue,
+      status: selectValue,
+    });
+  }
 }
--- src/app/accounts/home-accounts/home-accounts.component.ts
+++ src/app/accounts/home-accounts/home-accounts.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { AddAccountComponent } from '../add-account/add-account.component';
+import { ItemAccountComponent } from '../item-account/item-account.component';
 
 @Component({
   selector: 'app-home-accounts',
-  imports: [],
+  imports: [AddAccountComponent, ItemAccountComponent],
   templateUrl: './home-accounts.component.html',
-  styleUrl: './home-accounts.component.css'
+  styleUrl: './home-accounts.component.css',
 })
 export class HomeAccountsComponent {
+  tabAccounts = [
+    {
+      name: 'Wassim Account',
+      status: 'active',
+    },
+    {
+      name: 'Najwa Account',
+      status: 'inactive',
+    },
+  ];
 
+  addAccount(newAccount) {
+    this.tabAccounts.push(newAccount);
+  }
 }
