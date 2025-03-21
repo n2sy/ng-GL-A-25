b8f8542d525ae1f1b22fdbf5121e0ad15ab31eaa
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-03-21 22:26:41 +0100

    ajouter short pipe

--- src/app/manage-servers/manage-servers.component.ts
+++ src/app/manage-servers/manage-servers.component.ts
@@ -1,11 +1,51 @@
+import {
+  DatePipe,
+  NgClass,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
 import { Component } from '@angular/core';
+import { ShortPipe } from '../pipes/short.pipe';
 
 @Component({
   selector: 'app-manage-servers',
-  imports: [],
+  imports: [NgClass, UpperCasePipe, TitleCasePipe, DatePipe, ShortPipe],
   templateUrl: './manage-servers.component.html',
-  styleUrl: './manage-servers.component.css'
+  styleUrl: './manage-servers.component.css',
 })
 export class ManageServersComponent {
+  tabServers = [
+    {
+      name: 'Production Server',
+      type: 'small',
+      date_d: new Date(2020, 6, 5),
+      status: 'critical',
+    },
+    {
+      name: 'Development Test Server',
+      type: 'large',
+      date_d: new Date(2020, 6, 5),
+      status: 'stable',
+    },
+    {
+      name: 'Development Server',
+      type: 'medium',
+      date_d: new Date(2022, 2, 10),
+      status: 'offline',
+    },
+    {
+      name: 'Nidhal Server',
+      type: 'small',
+      date_d: new Date(2020, 6, 5),
+      status: 'stable',
+    },
+  ];
 
+  affecterClass(st) {
+    return {
+      'list-group-item-success': st == 'stable',
+      'list-group-item-warning': st == 'offline',
+      'list-group-item-danger': st == 'critical',
+    };
+  }
 }
